9ce75334a153d638f9af48b405a7d66921585d77
siso54@gmail.com <siso54@gmail.com>
2024-02-01 13:57:32 +0100

    display testing

--- esphome/components/waveshare_epaper/waveshare_420v2.cpp
+++ esphome/components/waveshare_epaper/waveshare_420v2.cpp
@@ -158,7 +158,7 @@ void WaveshareEPaper4P2InV2::partial_update_() {
     this->set_timeout(100, [this] {
       this->wait_until_idle_();
       this->write_buffer_(WRITE_BUFFER, 0, this->get_height_internal());
-      END(ON_PARTIAL);
+      SEND(ON_PARTIAL);
       this->command(ACTIVATE);  // don't wait here
       this->is_busy_ = false;
     });
