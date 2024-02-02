57c655f899e13020b64a2dc7d4bb76e59917a88b
siso54@gmail.com <siso54@gmail.com>
2024-02-02 09:26:02 +0100

    display testing

--- esphome/components/waveshare_epaper/waveshare_420v2.cpp
+++ esphome/components/waveshare_epaper/waveshare_420v2.cpp
@@ -133,8 +133,8 @@ void WaveshareEPaper4P2InV2::setup() {
   ESP_LOGI(TAG, "Setting up.");
   SEND(DRV_OUT_CTL);
   SEND(BORDER_FULL);
-  // this->command(0x1A);
-  // this->data(0x5A);
+  this->command(0x1A);
+  this->data(0x5A);
   this->command(0x22);
   this->data(0x99);
   this->command(ACTIVATE);
@@ -164,7 +164,7 @@ void WaveshareEPaper4P2InV2::partial_update_() {
   ESP_LOGI(TAG, "Performing partial e-paper update.");
   this->set_timeout(100, [this] {
     // this->write_lut_(FULL_LUT);
-    // SEND(BORDER_PART);
+    SEND(BORDER_PART);
     SEND(UPSEQ);
     this->command(ACTIVATE);
     this->set_timeout(100, [this] {
@@ -180,12 +180,12 @@ void WaveshareEPaper4P2InV2::partial_update_() {
 void WaveshareEPaper4P2InV2::full_update_() {
   ESP_LOGI(TAG, "Performing full e-paper update.");
   // this->write_lut_(FULL_LUT);
-  // SEND(BORDER_FULL);
+  SEND(BORDER_FULL);
   SEND(UPSEQ);
   this->command(ACTIVATE);
   this->wait_until_idle_();
   this->write_buffer_(WRITE_BUFFER, 0, this->get_height_internal());
-  this->write_buffer_(WRITE_BASE, 0, this->get_height_internal());
+  // this->write_buffer_(WRITE_BASE, 0, this->get_height_internal());
   SEND(ON_FULL);
   this->command(ACTIVATE);  // don't wait here
   this->is_busy_ = false;
